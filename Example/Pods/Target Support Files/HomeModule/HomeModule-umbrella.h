b48bbb7248ae1f8d5762845a91f733dbf9137f9a
everyStudy_Now <984013284@qq.com>
2020-04-15 16:03:57 +0800

    首次提交

---
+++ Example/Pods/Target Support Files/HomeModule/HomeModule-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "HomeModule.h"
+
+FOUNDATION_EXPORT double HomeModuleVersionNumber;
+FOUNDATION_EXPORT const unsigned char HomeModuleVersionString[];
+
